44988e8207be6c5beaa1bb18e61eac5f70f63aae
Rodrigo Reyes <rodrigo.reyes@laureate.net>
2016-09-28 08:30:57 -0400

    morning

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,162 @@
-// Freelancer Theme JavaScript
 
-(function($) {
-    "use strict"; // Start of use strict
+$ = jQuery.noConflict();
+window.Modernizr = {};
+var currRefId;
+var pStatus;
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+function jsCallbackReady(objectId) {
+    console.log(objectId + " is ready!!");
+    window.kdp = document.getElementById(objectId);
+
+    kdp.kBind("mediaReady.init", function () {
+        $('.totalTime').text(timeConversion(kdp.evaluate("{mediaProxy.entry.duration}")));
+        console.log("duration ready: " + kdp.evaluate("{mediaProxy.entry.duration}"));
+        kdp.kUnbind('.init');
+        kdp.kBind('playerUpdatePlayhead', 'videoTimeUpdateHandler');
+        kdp.kBind('playerStateChange', 'playerStateHandler');
+
+        //$('.hidePlayer').hide();
+
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
+
+}
+
+
+
+$( document ).ready(function() {
+
+$.getJSON('js/data.json', function(data) {
+    var template = document.getElementById('figureTemplate').innerHTML;
+    $.each(data.items, function(i, item) {
+        var html = Mustache.to_html(template, item);
+       $('.grid').append(html);
     });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+});
+$('body').on('click', '.clickFigure', function(e) {
+  e.preventDefault();
+  var refId=$(this).data('target');
+  if (refId==currRefId){
+    console.log (e.target)
+     playPauseHandler();
+  }
+  else {
+       kdp.sendNotification("changeMedia", { "referenceId" : refId });
+       currRefId=refId;
+  }
+});
 
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
+});
 
-})(jQuery); // End of use strict
+
+function assignEvent  (){
+$( ".clickFigure" ).click(function(e) {
+
+});
+}
+
+
+// buttons`
+$( "#playBtn" ).click(function(e) {
+  e.preventDefault();
+  playPauseHandler();
+});
+
+function playPauseHandler(){
+  if (pStatus=='playing')
+  {
+    kdp.sendNotification("doPause");
+  }
+  else if (pStatus=='paused') {
+      kdp.sendNotification("doPlay");
+  }
+}
+
+
+$( "#pauseBtn" ).click(function(e) {
+  e.preventDefault();
+  kdp.sendNotification("doPause");
+});
+//videoPlayer events
+function videoTimeUpdateHandler(data, id) {
+  var currentTime = data;
+  var duration=kdp.evaluate("{mediaProxy.entry.duration}")
+    $('.currentTime').text(timeConversion(data));
+    var remainingTime=duration-data;
+    $('.totalTime').text('-'+timeConversion(remainingTime));
+    var percent = currentTime/kdp.evaluate("{mediaProxy.entry.duration}");
+      updateProgressWidth(percent);
+}
+
+
+
+function playerStateHandler(e){
+if (e=='paused'|| e=='playing')
+{
+  pStatus=e;
+  console.log(pStatus+'<<<__')
+}
+}
+
+
+
+function timeConversion(seconds) {
+  d = Number(seconds);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+  return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+}
+
+
+//scrub bar events
+
+
+
+
+function updateProgressWidth(percent) {
+        $('.timebar').width((percent * 100) + "%");
+
+    }
+
+
+var timeDrag = false;   /* Drag status */
+$('.progressbar').mousedown(function(e) {
+    timeDrag = true;
+    updatebar(e.pageX);
+});
+$(document).mouseup(function(e) {
+    if(timeDrag) {
+        timeDrag = false;
+        updatebar(e.pageX);
+    }
+});
+$(document).mousemove(function(e) {
+    if(timeDrag) {
+        updatebar(e.pageX);
+    }
+});
+
+//update Progress Bar control
+var updatebar = function(x) {
+    var progress = $('.progressBar');
+    var maxduration = kdp.evaluate("{mediaProxy.entry.duration}")//Video duraiton
+    var position = x - progress.offset().left; //Click pos
+    var percentage = 100 * position / progress.width();
+
+    //Check within range
+    if(percentage > 100) {
+        percentage = 100;
+    }
+    if(percentage < 0) {
+        percentage = 0;
+    }
+
+    //Update progress bar and video currenttime
+    var   seekTime = maxduration * percentage / 100;
+
+    $('.timeBar').css('width', percentage+'%');
+    kdp.sendNotification("doSeek", seekTime);
+};
